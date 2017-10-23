700d4e0becec5a0bfb6533b4d0ae6ec46194b8a6
Antoni Matusz <antoni.matusz@itdirection.pl>
2017-10-23 22:01:50 +0200

    trending-section

--- app/script/scripts.js
+++ app/script/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('.bxslider').bxSlider({
 		controls: false,
 		auto: true,
+		onSliderLoad: function(){
+			$('.trending-slider-container .bx-wrapper').css( "maxWidth", "99%" );
+		}
 	});
 
 	$('.slider-text-content').on("click", "button", function(){
