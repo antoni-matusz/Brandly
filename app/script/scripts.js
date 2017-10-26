7d8031e24b0b5dd06495b69b97ea458da64a160b
Antoni Matusz <antoni.matusz@itdirection.pl>
2017-10-26 21:02:40 +0200

    finalResult

--- app/script/scripts.js
+++ app/script/scripts.js
@@ -17,5 +17,40 @@ $(document).ready(function(){
     window.location.replace("/shop");
 	});
 
+	function showCurrentYear() {
+		var d = new Date();
+		var n = d.getFullYear();
+		$("#time").text(n);
+	}
+
+	showCurrentYear();
+
+	function sendEmail(){
+		var form = '.subscribe-section form';
+		$(form).on('submit',function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+			var email = $(form + ' input[type="email"]').val();
+
+			$.ajax({
+				url: "mail.php",
+				type: 'POST',
+				data: {
+					email: email,
+				},
+				success: function(result){
+					if(result == 'email=true'){
+						$(form + ' label').text('You have subscribed our newsletter').css({"color": "green"});
+						$(form + ' input[type="email"]').val('');
+					}else{
+						$(form + ' input[type="submit"]').val('ERROR').css({"color": "red"});
+						location.reload();
+					}
+				}
+			});
+		}); 
+	}
+
+	sendEmail();
 
 });
